4e67aa5ac1e06edb415840ac2c1fbfe478e41e68
Le Duc Anh <58109324+TheRookie113@users.noreply.github.com>
2022-09-10 17:22:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "localhost",
+                  "task-c-db.cclmcpqknmoe.us-east-1.rds.amazonaws.com",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
